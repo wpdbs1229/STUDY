20fe9b87fab4833d7bb0fbca03a54687f97dc6eb
persimmon tree <56255240+wpdbs1229@users.noreply.github.com>
2021-11-14 15:58:36 +0900

    Update Phonenumber.java

--- Phonenumber.java
+++ Phonenumber.java
@@ -4,11 +4,11 @@ public class Phonenumber {
 	public String starnum(String phone_number) {
 		
 		
-		StringBuilder answer = new StringBuilder(phone_number);
+		StringBuilder answer = new StringBuilder(phone_number);  // 전화번호를 받아 StringBuilder로 객체를 생성
 		for(int i =0 ; i<phone_number.length()-4; i++)
-			answer.setCharAt(i, '*');
+			answer.setCharAt(i, '*');   //setCharAt로 원하는 인덱스의 값을 *로 바꿔줍니다.
 		
-		return answer.toString();
+		return answer.toString(); //바꾼 전화번호를 반환합니다.
 		
 	}
 	public static void main(String[] args) {
@@ -25,3 +25,5 @@ public class Phonenumber {
 	}
 
 }
+
+// 개인정보 보호를 위해 고객의 전화번호를 끝자리 4자리만 남기고 *로 바꾸는 코드 입니다.
