a09a6ac26b7accf067fdd451386c6fb593ca090e
persimmon tree <56255240+wpdbs1229@users.noreply.github.com>
2023-02-06 11:13:56 +0900

    Add files via upload

---
+++ Programmers/Lv0/ArrayLength.java
@@ -0,0 +1,12 @@
+public class ArrayLength {
+    public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+
+        int idx =0;
+        for (String s : strlist){
+            answer[idx++] = s.length();
+        }
+
+        return answer;
+    }
+}
---
+++ Programmers/Lv0/BacterialGrowth.java
@@ -0,0 +1,9 @@
+public class BacterialGrowth {
+    public int solution(int n, int t) {
+        int answer = 0;
+        for (int i = 0; i <t; i++) {
+            n *=2;
+        }
+        return answer = n;
+    }
+}
